c48e301c006ba7b12c0f8567b0aee68fa6149655
Gabriel Ramos <gramos.sk9@gmail.com>
2023-11-27 19:51:23 -0300

    feat: creation of the project base and its components creating the basic project and adding the components

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const ExpenseCalculator = () => {
+ const [expenses, setExpenses] = useState([]);
+ const [name, setName] = useState('');
+ const [amount, setAmount] = useState('');
+ const [type, setType] = useState('');
+ const [payPer, setPayPer] = useState('');
+
+ const addExpense = (e) => {
+    e.preventDefault();
+    setExpenses([...expenses, { name, amount, type, payPer }]);
+    setName('');
+    setAmount('');
+    setType('');
+    setPayPer('');
+ };
+
+ const totalExpenses = expenses.reduce((total, expense) => {
+    if (expense.type === 'Expense') {
+      return total + parseFloat(expense.amount);
+    }
+    return total;
+ }, 0);
+
+ const totalIncome = expenses.reduce((total, expense) => {
+    if (expense.type === 'Income') {
+      return total + parseFloat(expense.amount);
+    }
+    return total;
+ }, 0);
+
+ return (
+    <div>
+      <h2>Expense Calculator</h2>
+      <form onSubmit={addExpense}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+        <label>
+          Amount:
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+        </label>
+        <label>
+          Type:
+          <select value={type} onChange={(e) => setType(e.target.value)}>
+            <option value="">Select...</option>
+            <option value="Expense">Expense</option>
+            <option value="Income">Income</option>
+          </select>
+        </label>
+        <label>
+          Pay Per:
+          <select value={payPer} onChange={(e) => setPayPer(e.target.value)}>
+            <option value="">Select...</option>
+            <option value="Pix">Pix</option>
+            <option value="Cartão1">Cartão de credito</option>
+            <option value="Cartão2">Cartão de debito</option>
+            <option value="Boleto">Boleto</option>
+          </select>
+        </label>
+        <button type="submit">Add Expense</button>
+      </form>
+      <h3>Total Expenses: {totalExpenses}</h3>
+      <h3>Total Income: {totalIncome}</h3>
+      <h3>Total Profit: {totalIncome - totalExpenses}</h3>
     </div>
-  );
-}
+ );
+};
 
-export default App;
+export default ExpenseCalculator;
