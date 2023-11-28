5067e386df7c294c736a17f6f92d060ae00248c3
Gabriel Ramos <gramos.sk9@gmail.com>
2023-11-27 21:04:52 -0300

    feat: language change and list creation

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,72 +1,105 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const ExpenseCalculator = () => {
- const [expenses, setExpenses] = useState([]);
- const [name, setName] = useState('');
- const [amount, setAmount] = useState('');
- const [type, setType] = useState('');
- const [payPer, setPayPer] = useState('');
+  const [expenses, setExpenses] = useState([]);
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
+  const [type, setType] = useState("");
+  const [payPer, setPayPer] = useState("");
+  const [addedExpenses, setAddedExpenses] = useState([]);
 
- const addExpense = (e) => {
+  useEffect(() => {
+    const storedExpenses = localStorage.getItem("expenses");
+    if (storedExpenses) {
+      setExpenses(JSON.parse(storedExpenses));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+    setAddedExpenses(expenses);
+  }, [expenses]);
+
+  const addExpense = (e) => {
     e.preventDefault();
-    setExpenses([...expenses, { name, amount, type, payPer }]);
-    setName('');
-    setAmount('');
-    setType('');
-    setPayPer('');
- };
+    const newExpense = { name, amount, type, payPer };
+    setExpenses([...expenses, newExpense]);
+    setName("");
+    setAmount("");
+    setType("");
+    setPayPer("");
+  };
 
- const totalExpenses = expenses.reduce((total, expense) => {
-    if (expense.type === 'Expense') {
+  const totalExpenses = expenses.reduce((total, expense) => {
+    if (expense.type === "Expense") {
       return total + parseFloat(expense.amount);
     }
     return total;
- }, 0);
+  }, 0);
 
- const totalIncome = expenses.reduce((total, expense) => {
-    if (expense.type === 'Income') {
+  const totalIncome = expenses.reduce((total, expense) => {
+    if (expense.type === "Income") {
       return total + parseFloat(expense.amount);
     }
     return total;
- }, 0);
+  }, 0);
 
- return (
-    <div>
+  return (
+    <div className="container">
       <h2>Expense Calculator</h2>
       <form onSubmit={addExpense}>
         <label>
-          Name:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          Nome:
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
         <label>
-          Amount:
-          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} />
+          Valor:
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
         </label>
         <label>
-          Type:
+          Tipo:
           <select value={type} onChange={(e) => setType(e.target.value)}>
             <option value="">Select...</option>
-            <option value="Expense">Expense</option>
-            <option value="Income">Income</option>
+            <option value="Expense">Despesas</option>
+            <option value="Income">Receita</option>
           </select>
         </label>
         <label>
-          Pay Per:
+          Pagamento:
           <select value={payPer} onChange={(e) => setPayPer(e.target.value)}>
             <option value="">Select...</option>
             <option value="Pix">Pix</option>
-            <option value="Cartão1">Cartão de credito</option>
-            <option value="Cartão2">Cartão de debito</option>
+            <option value="CartãoCredito">Cartão de credito</option>
+            <option value="CartãoDebito">Cartão de debito</option>
             <option value="Boleto">Boleto</option>
           </select>
         </label>
-        <button type="submit">Add Expense</button>
+        <button type="submit">Adicionar</button>
       </form>
-      <h3>Total Expenses: {totalExpenses}</h3>
-      <h3>Total Income: {totalIncome}</h3>
-      <h3>Total Profit: {totalIncome - totalExpenses}</h3>
+      <h3>Total Despesas: {totalExpenses}</h3>
+      <h3>Total Receita: {totalIncome}</h3>
+      <h3>Total Lucro: {totalIncome - totalExpenses}</h3>
+
+      <h3>List of Added Expenses:</h3>
+      <ul>
+        {addedExpenses.map((expense, index) => (
+          <li key={index}>
+            Name: {expense.name}, Amount: {expense.amount}, Type: {expense.type}, Pay Per: {expense.payPer}
+          </li>
+        ))}
+      </ul>
     </div>
- );
+  );
 };
 
 export default ExpenseCalculator;
+
